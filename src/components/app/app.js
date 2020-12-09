dbe5e528810a668f4d75addbcae447d76f91bcd2
Evgeny Ovodkov <kefir4ek@gmail.com>
2020-12-09 22:39:30 +0300

    New PostItem class

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,6 +9,13 @@ import PostAddForm from '../post-add-form/post-add-form';
 import './app.css';
 
 const App = () => {
+
+	const data = [
+		{label: 'Going to learn React', important: true, id: 'jkh'},
+		{label: 'That is so good', important: false, id: 'dgn'},
+		{label: 'I need a break...', important: false, id: 'jklk'}
+	];
+
 	return (
 		<div className="app">
 			<AppHeader/>
@@ -16,7 +23,7 @@ const App = () => {
 				<SearchPanel/>
 				<PostStatusFilter/>
 			</div>
-			<PostList/>
+			<PostList posts={data}/>
 			<PostAddForm/>
 		</div>
 	)
